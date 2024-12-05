ba3252e4b94764e6e6e1fb59dc0892606fa35524
Monique Graham <magraham@uchicago.edu>
2024-12-04 23:20:35 -0600

    update4

--- app.R
+++ app.R
@@ -1,12 +1,10 @@
-https://moniquepset2.shinyapps.io/my_app/
-  
 setwd("C:/Users/momo_/OneDrive/Documents/GitHub/finalproject")
 
-#Question 2
 library(shiny)
 library(tidyverse)
 library(plotly)
-data <- read.csv("clean_chicago_data.csv")
+
+data <- read.csv("C:/Users/momo_/OneDrive/Documents/GitHub/finalproject/clean_chicago_data.csv")
 ui <- fluidPage(
   
   titlePanel("Homelessness in Chicago: Affordable Housing vs. Vacant Lots"),
@@ -38,8 +36,7 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  path <- "C:/Users/momo_/OneDrive/Documents/GitHub/finalproject"
-  data <- read_csv("clean_chicago_data.csv")
+  data <- read_csv("C:/Users/momo_/OneDrive/Documents/GitHub/finalproject/clean_chicago_data.csv")
   
   output$combined <- renderText(paste({input$neigh}, "and", {input$neigh1}))
   output$property_type_t <- renderText({input$property_type})
@@ -52,21 +49,32 @@ server <- function(input, output) {
     filter(data, property_type == input$property_type & (community == input$neigh | community == input$neigh1))
   })
   
+  long_data <- reactive({
+    info() %>%
+      pivot_longer(cols = c(count_vacant, affordable_units_no),
+                   names_to = "Metrics",
+                   values_to = "Count") 
+  })
   
   output$ts <- renderPlotly({
-    ggplotly(ggplot(data = info()) +
-               geom_bar(aes(community, count_vacant, fill = community), stat = "identity") +
-               labs(title = paste("Vacant Lots in", input$neigh, "and", input$neigh1), 
-                    y = "Count",
-                    fill = "Community Areas") +
-               theme(axis.text.x = element_text(angle=45, vjust=.5, hjust=1))
-    )
+    ggplot(data = long_data()) +
+      geom_bar(aes(x = community, y = Count, fill = Metrics), stat = "identity", position = "dodge") +
+      scale_y_continuous(
+        name = "Count",
+        sec.axis = sec_axis(~ . / 10, name = "Affordable Units") 
+      ) +
+      scale_fill_manual(values = c("count_vacant" = "cyan", "affordable_units_no" = "pink"),
+                        labels = c("Vacant Lots", "Affordable Housing")) +
+      labs(title = paste("Vacant Lots and Affordable Units in", input$neigh, "and", input$neigh1), 
+           x = "Community Areas",
+           fill = "Metrics") +
+      theme(axis.text.x = element_text(angle = 45, vjust = .5, hjust = 1))
   })
   
   output$ts1 <- renderPlotly({
     ggplotly(ggplot(data = info1()) +
-               geom_bar(aes(community, affordable_units_no), stat = "identity") +
-               labs(title = paste(input$property_type, "in", input$neigh, "and", input$neigh1), 
+               geom_bar(aes(x = community, y = affordable_units_no), stat = "identity") +
+               labs(title = paste(input$property_type, "Affordable Housing Units in", input$neigh, "and", input$neigh1), 
                     x = "Community Areas", 
                     y = "Total Number of Affordable Units")
     )
@@ -76,4 +84,15 @@ server <- function(input, output) {
 
 shinyApp(ui = ui, server = server)
 
+output$ts <- renderPlotly({
+  ggplotly(ggplot(data = info()) +
+             geom_col(aes(x = community, y = count_vacant, fill = "Vacant Lots"), stat = "identity", position = "dodge") +
+             geom_col(aes(x = community, y = affordable_units_no, fill = "Affordable Units"), stat = "identity", position = "dodge") +
+             labs(title = paste("Vacant Lots in", input$neigh, "and", input$neigh1), 
+                  x = "Community Areas",
+                  y = "Count",
+                  fill = "Community Areas") +
+             theme(axis.text.x = element_text(angle=45, vjust=.5, hjust=1))
+  )
+})
 
